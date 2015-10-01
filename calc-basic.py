3ea5da48c1f18a8211ded3dce00cee9b51b69227
Marek Denis <marek.denis@gmail.com>
2015-10-01 23:04:50 +0200

    Remove irrelevant comments
    With the enhancements and modifications they are no longer relevant.

--- calc-basic.py
+++ calc-basic.py
@@ -127,26 +127,15 @@ class Interpreter(object):
 
     def expr(self):
         """expr -> INTEGER PLUS INTEGER"""
-        # set current token to the first token taken from the input
         self.current_token = self.get_next_token()
 
-        # we expect the current token to be a single-digit integer
         left = self.current_token
         self.eat([INTEGER])
-        # we expect the current token to be a '+' token
         op = self.current_token
         self.eat(OPERATORS.keys())
 
-        # we expect the current token to be a single-digit integer
         right = self.current_token
         self.eat([INTEGER])
-        # after the above call the self.current_token is set to
-        # EOF token
-
-        # at this point INTEGER PLUS INTEGER sequence of tokens
-        # has been successfully found and the method can just
-        # return the result of adding two integers, thus
-        # effectively interpreting client input
 
         if op.type in OPERATORS:
             return OPERATORS[op.type](left.value, right.value)
